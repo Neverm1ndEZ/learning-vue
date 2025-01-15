5a0852a4e284a661032ab580f810dd8277185943
Neverm1ndEZ <mihirsuman1@gmail.com>
2025-01-15 23:59:22 +0530

    feat: todo to understand forms and events

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 	const name = ref("John Doe");
 	const status = ref("active");
 	const tasks = ref(["task 1", "task 2", "task 3"]);
+	const newTask = ref("");
 
 	const toggleStatus = () => {
 		if (status.value === "active") {
@@ -15,8 +16,19 @@
 			status.value = "active";
 		}
 	};
-</script>
 
+	const addTask = () => {
+		if (newTask.value.trim() !== "") {
+			tasks.value.push(newTask.value);
+			newTask.value = "";
+		}
+	};
+
+	const deleteTask = (index) => {
+		tasks.value.splice(index, 1);
+	};
+</script>
+xs
 <template>
 	<h1>Vue Crash Course</h1>
 	<h2>{{ name }}</h2>
@@ -24,9 +36,24 @@
 	<p v-else-if="status === 'pending'">User is pending</p>
 	<p v-else>User is not active</p>
 
+	<form @submit.prevent="addTask">
+		<label for="newTask">Add Task</label>
+		<input
+			type="text"
+			id="newTask"
+			name="newTask"
+			placeholder="add task"
+			v-model="newTask"
+		/>
+		<button type="submit">Add Task</button>
+	</form>
+
 	<h3>Tasks</h3>
 	<ul>
-		<li v-for="task in tasks" :key="task">{{ task }}</li>
+		<li v-for="(task, index) in tasks" :key="task">
+			<span>{{ task }}</span>
+			<button @click="deleteTask(index)">X</button>
+		</li>
 	</ul>
 
 	<!-- <button v-on:click="toggleStatus">Change Status</button> -->
