3cbe43b9d24631f5bc3f691f389355a2adea01b0
Neverm1ndEZ <mihirsuman1@gmail.com>
2025-01-16 02:40:57 +0530

    feat: tailwind-setup + pages-ui setup + similar react concept transition

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,15 @@
-<!-- Composition API -->
-
 <script setup>
-	import { onMounted, ref } from "vue";
-	const name = ref("John Doe");
-	const status = ref("active");
-	const tasks = ref(["task 1", "task 2", "task 3"]);
-	const newTask = ref("");
-
-	const toggleStatus = () => {
-		if (status.value === "active") {
-			status.value = "pending";
-		} else if (status.value === "pending") {
-			status.value = "inactive";
-		} else {
-			status.value = "active";
-		}
-	};
-
-	const addTask = () => {
-		if (newTask.value.trim() !== "") {
-			tasks.value.push(newTask.value);
-			newTask.value = "";
-		}
-	};
-
-	const deleteTask = (index) => {
-		tasks.value.splice(index, 1);
-	};
-
-	// Lifecycle hook - data fetching
-	onMounted(async () => {
-		try {
-			const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-			const data = await res.json();
-			tasks.value = data.map((task) => task.title);
-		} catch (err) {
-			console.log(err);
-		}
-	});
+	import Hero from "./components/Hero.vue";
+	import HomeCards from "./components/HomeCards.vue";
+	import JobListings from "./components/JobListings.vue";
+	import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-	<h1>Vue Crash Course</h1>
-	<h2>{{ name }}</h2>
-	<p v-if="status === 'active'">User is active</p>
-	<p v-else-if="status === 'pending'">User is pending</p>
-	<p v-else>User is not active</p>
-
-	<form @submit.prevent="addTask">
-		<label for="newTask">Add Task</label>
-		<input
-			type="text"
-			id="newTask"
-			name="newTask"
-			placeholder="add task"
-			v-model="newTask"
-		/>
-		<button type="submit">Add Task</button>
-	</form>
-
-	<h3>Tasks</h3>
-	<ul>
-		<li v-for="(task, index) in tasks" :key="task">
-			<span>{{ task }}</span>
-			<button @click="deleteTask(index)">X</button>
-		</li>
-	</ul>
-
-	<!-- <button v-on:click="toggleStatus">Change Status</button> -->
-	<button @click="toggleStatus">Change Status</button>
+	<Navbar />
+	<!-- Eg of props -->
+	<!-- <Hero title="Test Title" subTitle="test sub" /> -->
+	<Hero />
+	<HomeCards />
+	<JobListings :limit="3" :showButton="true" />
 </template>
-
-<style scoped>
-	h1 {
-		color: red;
-	}
-</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+	import { defineProps } from "vue";
+	defineProps({
+		bg: {
+			type: String,
+			default: "bg-gray-100",
+		},
+	});
+</script>
+
+<template>
+	<div :class="`${bg} p-6 rounded-lg shadow-md`">
+		<!-- // This is where the content will be injected kinda like {children} in React -->
+		<slot></slot>
+	</div>
+</template>
