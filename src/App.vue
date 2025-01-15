7ff577ee9f7cc917fd1fab8d124a996670343f8d
Neverm1ndEZ <mihirsuman1@gmail.com>
2025-01-15 23:41:45 +0530

    feat: composition-api-init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<!-- Composition API -->
+
+<script setup>
+	import { ref } from "vue";
+	const name = ref("John Doe");
+	const status = ref("active");
+	const tasks = ref(["task 1", "task 2", "task 3"]);
+
+	const toggleStatus = () => {
+		if (status.value === "active") {
+			status.value = "pending";
+		} else if (status.value === "pending") {
+			status.value = "inactive";
+		} else {
+			status.value = "active";
+		}
+	};
+</script>
+
+<template>
+	<h1>Vue Crash Course</h1>
+	<h2>{{ name }}</h2>
+	<p v-if="status === 'active'">User is active</p>
+	<p v-else-if="status === 'pending'">User is pending</p>
+	<p v-else>User is not active</p>
+
+	<h3>Tasks</h3>
+	<ul>
+		<li v-for="task in tasks" :key="task">{{ task }}</li>
+	</ul>
+
+	<!-- <button v-on:click="toggleStatus">Change Status</button> -->
+	<button @click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped>
+	h1 {
+		color: red;
+	}
+</style>
---
+++ src/options.vue
@@ -0,0 +1,49 @@
+// options API (older vue shit)
+
+<script>
+	export default {
+		data() {
+			return {
+				name: "John Doe",
+				status: "pending",
+				tasks: ["task 1", "task 2", "task 3"],
+				link: "https://google.com",
+			};
+		},
+		methods: {
+			toggleStatus() {
+				if (this.status === "active") {
+					this.status = "pending";
+				} else if (this.status === "pending") {
+					this.status = "inactive";
+				} else {
+					this.status = "active";
+				}
+			},
+		},
+	};
+</script>
+
+<template>
+	<h1>Vue Crash Course</h1>
+	<h2>{{ name }}</h2>
+	<p v-if="status === 'active'">User is active</p>
+	<p v-else-if="status === 'pending'">User is pending</p>
+	<p v-else>User is not active</p>
+
+	<h3>Tasks</h3>
+	<ul>
+		<li v-for="task in tasks" :key="task">{{ task }}</li>
+	</ul>
+
+	<a :href="link">Google</a>
+
+	<!-- <button v-on:click="toggleStatus">Change Status</button> -->
+	<button @click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped>
+	h1 {
+		color: red;
+	}
+</style>
